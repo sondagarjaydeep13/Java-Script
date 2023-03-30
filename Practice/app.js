02339b716c00133cd05f2dab92ceb3e002c5d8a7
sondagarjaydeep13 <113520283+sondagarjaydeep13@users.noreply.github.com>
2023-03-30 13:25:40 +0530

    Java Script

---
+++ Practice/app.js
@@ -0,0 +1,32 @@
+// Replace and ReplaceAll
+
+// const string = "Hello World";
+// const newstring = string.replace("Hello", "Hii");
+// console.log(newstring); // Here output:Hii World
+// console.log(string); // output:Hello World
+
+// var string =
+//   "hii Lajk hii lak hi hii hii kale hii hii bms jkjs hii hii slslhi hii";
+// var newString = string.replaceAll("hii", "Hello");
+// console.log("Second :" + newString);
+// console.log("first :" + string);
+
+// string = newString;
+// console.log(newString);
+
+// toLowercase and toUppercase
+
+// const string = "hjg D jljdld EHDKHDKHD khk HDDKjhdkjdh ";
+// const result = string.toLowerCase();
+// const result2 = string.toUpperCase();
+// console.log(result2);
+
+const SKEY = "thisismykey";
+
+const user = "thIsISmykey";
+const user2 = user.toLowerCase();
+if (SKEY == user2) {
+  console.log("true");
+} else {
+  console.log("false");
+}
